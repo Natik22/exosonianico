a93782f79a1eea7e5b009b809a4b158a0266e881
Natik22 <nicolas.2205@hotmail.fr>
2024-01-09 10:19:14 +0100

    save080124

---
+++ main.js
@@ -0,0 +1,81 @@
+// let nombre = 54;
+// let usernumber = window.parseInt(prompt("entrer un nombre"));
+
+// // console.log (usernumber)
+
+// while (usernumber !== nombre ) { 
+//     usernumber = window.parseInt(prompt("entrer un nombre"));
+//     if (usernumber<nombre){
+//     alert ("Estimation trop basse");
+//     }
+//     else if (usernumber>nombre){
+//     alert ("Estimation trop haute");
+// }
+    
+// }
+//  if (usernumber===nombre){
+//     alert ("Bien joué")
+//  }
+
+// ----------------------------------------------------------------------------------
+
+// let min=1; 
+// let max=100;  
+// let nombre = Math.floor(Math.random() * (max - min)) + min; 
+// let usernumber = window.parseInt(prompt("entrer un nombre"));
+
+// while (usernumber !== nombre ) { 
+//     usernumber = window.parseInt(prompt("entrer un nombre"));
+//     if (usernumber<nombre){
+//     alert ("Estimation trop basse");
+//     }
+//     else if (usernumber>nombre){
+//     alert ("Estimation trop haute");
+// }
+    
+// }
+//  if (usernumber===nombre){
+//     alert ("Bien joué")
+//  }
+ 
+//  // Cette ligne génère aléatoirement un nombre entre 1 et 100
+//  let solution = Math.floor(Math.random() * 100) + 0;
+  
+//  // Décommentez temporairement cette ligne pour mieux vérifier le programme
+//   console.log("(La solution est " + solution + ")");
+  
+  
+//  let i;
+//  let nombreChoisi = Number(prompt("Choississez un nombre entre 1 et 100"));
+ 
+//  for (i = 1; i <= 4; i++) {
+//      // Si le nombre a été deviné
+//      if (solution === nombreChoisi) {
+//          prompt("Felicitations, c'était bien " + solution + " qu'il fallait trouver ! Tu as trouvé en " + i + " essais");
+//          console.log("Tu as trouvé " + solution + " en " + i + " essais");
+//          break;
+//      // Si le nombre est trop petit
+//      } else if (nombreChoisi < solution) {
+//          nombreChoisi = Number(prompt(nombreChoisi + " est trop petit"));
+//          console.log(nombreChoisi);
+//      // Si le nombre est trop grand
+//      } else if (nombreChoisi > solution) {
+//          nombreChoisi = Number(prompt(nombreChoisi + " est trop grand"));
+//          console.log(nombreChoisi);
+//      // Si ce n'est pas un nombre
+//      } else {
+//          nombreChoisi = Number(prompt("Je n'ai pas compris"));
+//          console.log(nombreChoisi);
+//      }
+//  }
+//      // Si le nombre n'a pas été trouvé au bout de 5 essais.
+//  if (nombreChoisi !== solution) {
+//      alert("Tu as fais tes " + i + " essais. La solution était " + solution);
+//      console.log("Perdu ! c'était " + solution);
+//  } else {
+//     alert("Felicitations, c'était bien " + solution + " qu'il fallait trouver ! Tu as trouvé en " + i + " essais"); }
+
+
+
+// -----------------------------------------------------------------------
+
